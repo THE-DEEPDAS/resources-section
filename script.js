56a2dcf989b9a458d4f73e4c8a641bea3bbde5cd
Deep Das <deepdblm@gmail.com>
2024-08-22 17:11:58 +0530

    final push

--- script.js
+++ script.js
@@ -60,3 +60,11 @@ document.addEventListener('DOMContentLoaded', function() {
     // Initial check
     checkVisibility();
 });
+
+const hamburger = document.getElementById('hamburger');
+    const navLinks = document.getElementById('nav-links');
+
+    hamburger.addEventListener('click', () => {
+        navLinks.classList.toggle('show');
+    });
+  
